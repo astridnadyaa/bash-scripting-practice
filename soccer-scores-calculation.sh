76408c8f8121201ffb8ff9e593e5ff3af4983c12
astridnadyaa <73452972+astridnadyaa@users.noreply.github.com>
2025-01-13 08:53:41 +0700

    update script

--- soccer-scores-calculation.sh
+++ soccer-scores-calculation.sh
@@ -1,3 +1,3 @@
 # Script that aggregate how many times each soccer team has won
 
-cat raw-data/soccer-scores.txt | cut -d "," -f 2 | tail -n +2 | sort | uniq -c > soccer-scores-calculation.txt
+cat raw-data/soccer-scores.txt | cut -d "," -f 2 | tail -n +2 | sort | uniq -c > output/soccer-scores-calculation.txt
