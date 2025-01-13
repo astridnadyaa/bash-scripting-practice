ebc0ec3bdaa7c2332dd1aa6aa1a57910721f376e
astridnadyaa <73452972+astridnadyaa@users.noreply.github.com>
2025-01-13 08:54:01 +0700

    update script

--- soccer-team-rename.sh
+++ soccer-team-rename.sh
@@ -1,3 +1,3 @@
 # Renaming multiple soccer team name
 
-cat raw-data/soccer-scores.txt | sed 's/Cherno/Cherno City/g' | sed 's/Arda/Arda United/g' > soccer-team-rename.txt
+cat raw-data/soccer-scores.txt | sed 's/Cherno/Cherno City/g' | sed 's/Arda/Arda United/g' > output/soccer-team-rename.txt
